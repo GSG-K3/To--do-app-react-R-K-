38b8d1041e91ffd27188ab5a9020a46a42a4c30e
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-26 17:02:07 +0200

    fix accessibility, change design and handle empty list #21 #9 #18

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React from "react";
 
 function TodoList(props) {
   const tasksArray = props.tasks;
-  // create li for each task and save em in new array
-  const todoList = tasksArray.map(element => {
-    if (element.checked === true) {
-      return (
-        <li style={{ textDecoration: 'line-through' }} key={element.id}>
-          {element.task}
-          <button onClick={() => props.remove(element.id)}> Delete</button>
-          <button onClick={() => props.complete(element.id)}> check </button>
-        </li>
-      );
-    } else {
-      return (
-        <li key={element.id}>
-          {element.task}
+  if (tasksArray.length === 0) {
+    return <div>There are no tasks to display</div>;
+  } else {
+    // create li for each task and save em in new array
+    const todoList = tasksArray.map((element) => {
+      if (element.checked === true) {
+        return (
+          <li
+            className="todo-item"
+            style={{ textDecoration: "line-through" }}
+            key={element.id}
+          >
+            {element.task}
+            <div className="buttons">
+              <button
+                className="list-btn"
+                onClick={() => props.remove(element.id)}
+              >
+                X
+              </button>
+              <button
+                className="list-btn"
+                onClick={() => props.complete(element.id)}
+              >
+                ✓
+              </button>
+            </div>
+          </li>
+        );
+      } else {
+        return (
+          <li key={element.id} className="todo-item">
+            {element.task}
+            <div className="buttons">
+              <button
+                className="list-btn"
+                onClick={() => props.remove(element.id)}
+              >
+                X
+              </button>
+              <button
+                className="list-btn"
+                onClick={() => props.complete(element.id)}
+              >
+                ✓
+              </button>
+            </div>
+          </li>
+        );
+      }
+    });
 
-          <button onClick={() => props.remove(element.id)}> Delete</button>
-          <button onClick={() => props.complete(element.id)}> check </button>
-        </li>
-      );
-    }
-  });
-
-  return <div>{todoList}</div>;
+    return (
+      <div>
+        <ul className="todo-list">{todoList}</ul>
+      </div>
+    );
+  }
 }
 export default TodoList;
